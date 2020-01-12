4ee5f13744fc611a16ce753c0c2bbed9d042b4bd
Cody Barr <cody.barr@gmail.com>
2020-01-11 19:18:19 -0500

    fix: 🐛 typo

--- src/folder/folder-router.js
+++ src/folder/folder-router.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const xss = require('xss')
-const FolderService = require('./Folder-service')
+const FolderService = require('./folder-service')
 
 const folderRouter = express.Router()
 const jsonParser = express.json()
